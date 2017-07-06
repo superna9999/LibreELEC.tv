d0b1ba3a3d86f9196d7f7fdd53929e641c5cc233
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-07-06 19:52:12 +0100

    TEMP: fixup rebase script again

--- tools/RPi/rpi-linux-rebase.sh
+++ tools/RPi/rpi-linux-rebase.sh
@@ -19,6 +19,7 @@ net: Add non-mainline source for rtl8192cu wlan
 net: Fix rtl8192cu build errors on other platforms
 ARM64: Fix build break for RTL8187\/RTL8192CU wifi
 fixup: rtl8192cu fixes from milhouse
+rtl8192: Fixup build
 "
 
 IFS=$'\n'
