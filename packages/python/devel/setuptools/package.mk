8919825fc2443260f31e477d37ec7ff16041b4e6
Neil Armstrong <narmstrong@baylibre.com>
2019-01-03 13:27:14 +0000

    Use python3 for setuptools and mako

--- packages/python/devel/setuptools/package.mk
+++ packages/python/devel/setuptools/package.mk
@@ -7,14 +7,14 @@ PKG_SHA256="ca8119dd5c2764a7d290518817de0b880d23d790913fcd797c02ad2aa39b8c41"
 PKG_LICENSE="OSS"
 PKG_SITE="https://pypi.org/project/setuptools"
 PKG_URL="https://github.com/pypa/setuptools/archive/v$PKG_VERSION.tar.gz"
-PKG_DEPENDS_HOST="Python2:host"
+PKG_DEPENDS_HOST="Python3:host"
 PKG_LONGDESC="Replaces Setuptools as the standard method for working with Python module distributions."
 PKG_TOOLCHAIN="manual"
 
 make_host() {
-  python bootstrap.py
+  python3 bootstrap.py
 }
 
 makeinstall_host() {
-  python setup.py install --prefix=$TOOLCHAIN
+  python3 setup.py install --prefix=$TOOLCHAIN
 }
