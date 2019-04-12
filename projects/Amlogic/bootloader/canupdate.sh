62e0378ba422d075a9aa50bf4ed8a8aa123a3a0b
chewitt <github@chrishewitt.net>
2019-04-12 08:22:10 +0000

    project: add common bootloader files

---
+++ projects/Amlogic/bootloader/canupdate.sh
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+# detect legacy kernel installs and abort to prevent upgrades
+if [ "$(uname -r)" = "3.14.29" ]; then
+  exit 1
+fi
+
+# allow upgrades between aarch64 and arm images
+PROJECT=$("$1" | cut -d. -f1)
+if [ "$1" = "${PROJECT}.aarch64" -o "$1" = "${PROJECT}.arm" ]; then
+  exit 0
+else
+  exit 1
+fi
